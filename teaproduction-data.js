28cc20f9fc537c9d32085b02944fa93fc377e1ba
DAN-Pd <147044969+DAN-Pd@users.noreply.github.com>
2023-10-05 18:40:40 +0800

    Add files via upload

---
+++ teaproduction-data.js
@@ -0,0 +1,42 @@
+const TeaProductionData = {
+	"type": "FeatureCollection",
+	"features": [
+        {
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [111.080895, 30.175826]
+			},
+			"properties": {
+				"id": 1,
+                "name":"宜昌市五峰土家族自治县五峰县",
+				"attribute": "产茶区"
+			}
+		},
+        {
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [113.100774, 29.386975]
+			},
+			"properties": {
+				"id": 2,
+                "name":"湖南省岳阳市",	
+				"attribute": "产茶区"
+			}
+		},
+        {
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [116.150164, 28.632255]
+			},
+			"properties": {
+				"id": 3,	
+                "name":"江西省九江市",
+				"attribute": "产茶区"
+			}
+		},
+
+        ]
+    };
